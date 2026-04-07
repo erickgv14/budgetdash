f53ac34fbd5c54962cbfea3e423ef3b67b907ad8
erickgv <123135662+erickgv14@users.noreply.github.com>
2026-04-07 13:31:07 -0400

    Add files via upload
    Updated manifest and sw files to reflect name change from budget-dash.html to index.html

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'budget-dash-v1';
 const ASSETS = [
-    './budget_tracker.html',
+    './index.html',
     './manifest.json',
     'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
 ];
